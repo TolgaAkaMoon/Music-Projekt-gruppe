745fcf2b1b8469c4b379e4851b494318327cb03e
Tolga Özdemir <4160141@RTS-365.dk>
2020-03-06 09:09:07 +0100

    add changes

--- assets/javascript/player.js
+++ assets/javascript/player.js
@@ -1,11 +1,68 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-/*   const productsJson = fetch('./assets/json/sange.json')
-  .then(function (response) {
-      return response.json();
-      
-  }) */
+    let ip = "192.168.8.100"
+    let user = "BVpJlymp0Nk6MzqSLTCUg1VQKBZc6qrpLS3kYdFn"
+    let color = 0 - 65535
+    let light = 5
+
+    setInterval(function () {
+      fetch(`http://192.168.8.100/api/${user}/lights/${light}/state`, {
+        method: 'put',
+        body: JSON.stringify({"hue": parseInt(document.querySelector(".seekerBright").value)}),
+      })
+      .then(res => res.json())
+      .then(state => { 
+        });
+    }, 100);
+
+
+/*     fetch(`/api`, {
+      method: "post",
+      body: {"devicetype":"my_hue_app#iphone peter"}¨
+      , "hue": 00000, "bri": parseInt(lightBri)
+    }) */
+
     
+  
+
+    fetch(`http://${ip}/api/${user}/lights/5`, {
+    })
+    .then(res => res.json())
+    .then(tracks => {
+      console.log(tracks)
+        document.querySelector(".test__1").textContent = tracks.name
+        setInterval(function () {
+          document.querySelector(".test__2").textContent = tracks.state.on
+        }, 500);
+        document.querySelector(".test__3").addEventListener("click", function() {
+          if (tracks.state.on == false) {
+            tracks.state.on = true
+          }
+          else {
+            tracks.state.on = false
+          }
+          fetch(`http://192.168.8.100/api/${user}/lights/${light}/state`, {
+            method: 'put',
+            body: JSON.stringify({"on": tracks.state.on, "bri": 100}),
+          })
+          .then(res => res.json())
+          .then(state => {  
+
+            });
+        });
+      })
+
+      document.querySelector(".seekerBright")
+
+
+
+
+
+
+
+
+
+
     var shuffleButton = document.querySelector(".functions__shuffle");
     var backButton = document.querySelector(".functions__back");
     var playButton = document.querySelector(".functions__play");
